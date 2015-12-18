d144d8cd2e97fbd411cfa356a709875940dde465
viktor <nikitashvictor@gmail.com>
2015-12-18 18:58:14 +0200

    minior fixes

--- Fabric/ServiceFabric.php
+++ Fabric/ServiceFabric.php
@@ -45,19 +45,19 @@ class ServiceFabric{
                 if(!isset($access_token)) {
                     return array('status' => 'error', 'msg' => 'deniedByUser');
                 }
-                $result = \UploadModels\DropBoxModel::uploadFile($access_token, $uploadFile);
+                $result = \UploadModels\DropBoxModel::uploadFile($access_token, $uploadFile, $fileId);
                 break;
             case 1:
                 if(!isset($access_token)) {
                     return array('status' => 'error', 'msg' => 'deniedByUser');
                 }
-                $result = \UploadModels\GoogleDriveModel::uploadFile($access_token, $uploadFile);
+                $result = \UploadModels\GoogleDriveModel::uploadFile($access_token, $uploadFile, $fileId);
                 break;
             case 2:
                 if(!isset($access_token)) {
                     return array('status' => 'error', 'msg' => 'deniedByUser');
                 }
-                $result = \UploadModels\BoxModel::uploadFile($access_token, $uploadFile);
+                $result = \UploadModels\BoxModel::uploadFile($access_token, $uploadFile, $fileId);
                 break;
         }
         return $result;
