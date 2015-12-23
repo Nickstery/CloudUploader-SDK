a475c7db2f2e9f9edb0f6999e8d33be42a393ed9
viktor <nikitashvictor@gmail.com>
2015-12-23 16:12:31 +0200

    Add reciever

---
+++ HttpReciever/HttpReceiver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HttpReceiver;
+use \Dropbox as dbx;
+
+class HttpRecieiver{
+
+    public static function get($name, $type){
+
+        switch($type){
+            case 'int':
+                return intval($_GET[$name]);
+            case 'string':
+                return htmlspecialchars(strip_tags($_GET[$name]));
+        }
+        return '';
+    }
+
+}
